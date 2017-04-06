1d664f628987c1f798e84f9a91f23f81484e330b
Vanessa Naylon <vnaylon@gmail.com>
2017-04-05 22:54:28 -0700

    Back on my shit

---
+++ assets/js/all-satellite-launch-locations-geojson.js
@@ -0,0 +1,344 @@
+var allSatports = [
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          63.305,
+          45.965
+        ]
+      },
+      "properties": {
+        "name": "Baikonur Cosmodrome"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          40.2125,
+          -2.938333
+        ]
+      },
+      "properties": {
+        "name": "Broglio Space Centre"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -80.527778,
+          28.455556
+        ]
+      },
+      "properties": {
+        "name": "Cape Canaveral Air Force Station"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          110.951057,
+          19.614354
+        ]
+      },
+      "properties": {
+        "name": "China Wenchang Spacecraft Launch Site"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          59.842222,
+          51.093889
+        ]
+      },
+      "properties": {
+        "name": "Dombarovsky"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -52.760556,
+          5.237222
+        ]
+      },
+      "properties": {
+        "name": "Guiana Space Centre"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -2.83581,
+          31.09951
+        ]
+      },
+      "properties": {
+        "name": "Hammaguir French Special Weapons Test Centre"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          53.954722,
+          35.258333
+        ]
+      },
+      "properties": {
+        "name": "Imam Khomeini Space Center"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          100.298333,
+          40.960556
+        ]
+      },
+      "properties": {
+        "name": "Jiuquan Satellite Launch Center"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          45.72,
+          48.586
+        ]
+      },
+      "properties": {
+        "name": "Kaspustin Yar Cosmodrome"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -80.65085,
+          28.524058
+        ]
+      },
+      "properties": {
+        "name": "Kennedy Space Center"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.478056,
+          37.843333
+        ]
+      },
+      "properties": {
+        "name": "Mid-Atlantic Regional Spaceport"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          127.535069,
+          34.431867
+        ]
+      },
+      "properties": {
+        "name": "Naro Space Center"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          167.743,
+          9.047861
+        ]
+      },
+      "properties": {
+        "name": "Omelek Island"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -152.337778,
+          57.435833
+        ]
+      },
+      "properties": {
+        "name": "Pacific Spaceport Complex"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          34.690556,
+          31.897778
+        ]
+      },
+      "properties": {
+        "name": "Palmachim Air Force Base"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          40.577778,
+          62.925556
+        ]
+      },
+      "properties": {
+        "name": "Plestsk Cosmodrome"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          80.230425,
+          13.719939
+        ]
+      },
+      "properties": {
+        "name": "Satish Dhawan Space Centre"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          124.705,
+          39.66
+        ]
+      },
+      "properties": {
+        "name": "Sohae Satellite Launching Station"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          128,
+          51.7
+        ]
+      },
+      "properties": {
+        "name": "Svobodny Cosmodrome"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          111.608,
+          38.8491
+        ]
+      },
+      "properties": {
+        "name": "Taiyuan Satellite Launch Center"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          130.97,
+          30.4
+        ]
+      },
+      "properties": {
+        "name": "Tanegashima Space Center"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          131.081944,
+          31.251944
+        ]
+      },
+      "properties": {
+        "name": "Uchinoura Space Center"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -120.568056,
+          34.732778
+        ]
+      },
+      "properties": {
+        "name": "Vandenberg Air Force Base"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          128.333932,
+          51.884395
+        ]
+      },
+      "properties": {
+        "name": "Vostochny Cosmodrome"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.457222,
+          37.938611
+        ]
+      },
+      "properties": {
+        "name": "Wallops Flight Facility"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          136.5322,
+          -30.9553
+        ]
+      },
+      "properties": {
+        "name": "Woomera Prohibited Area"
+      }
+    }, {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          102.026556,
+          28.246017
+        ]
+      },
+      "properties": {
+        "name": "Xichang Satellite Launch Center"
+      }
+    }
+  ]
+}
+];
---
+++ assets/js/satellitespaceports.js
@@ -0,0 +1,10 @@
+var satelliteSpaceports = {
+    "type": "Feature",
+    "properties": {
+        "name": "Baikonur Cosmodrome"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [45.965, 63.305]
+    }
+};
